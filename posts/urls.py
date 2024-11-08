18f2ff3681d3094b3f5cd7a3b4ae19edf1192046
AkramovFozil <fozil.9909@gmail.com>
2024-11-08 17:09:27 +0500

    posts appga urls yaratildi.

---
+++ posts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from views import PostEditView, PostCreateView, PostListView, PostDetailView, PostDeleteView
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/create/', PostCreateView.as_view(), name='post-create'),
+    path('post/edit/<int:pk>/', PostEditView.as_view(), name='post-edit'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
+]
