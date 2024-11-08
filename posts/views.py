c003680ddac935954dcf7d9d440ee7b6c721d6df
AkramovFozil <fozil.9909@gmail.com>
2024-11-08 17:03:00 +0500

    posts appga views yaratildi.

--- posts/views.py
+++ posts/views.py
@@ -1,47 +1,41 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views import View
+from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostCreateForm, PostEditForm
 
 
-class PostListView(View):
-    def get(self, request):
-        posts = Post.objects.all()
-        return render(request, 'all-posts.html', {'posts': posts})
+class PostListView(ListView):
+    model = Post
+    template_name = 'all-posts.html'
+    context_object_name = 'posts'
 
 
-class PostDetailView(View):
-    def get(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
-        return render(request, 'show-post.html', {'post': post})
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'show-post.html'
+    context_object_name = 'post'
 
 
-class PostCreateView(LoginRequiredMixin, View):
-    def get(self, request):
-        form = PostCreateForm()
-        return render(request, 'create-post.html', {'form': form})
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostCreateForm
+    template_name = 'create-post.html'
+    success_url = reverse_lazy('post_list')
 
-    def post(self, request):
-        form = PostCreateForm(request.POST, request.FILES)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('post-list')
-        return render(request, 'create-post.html', {'form': form})
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
-class PostEditView(LoginRequiredMixin, View):
-    def get(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
-        form = PostEditForm(instance=post)
-        return render(request, 'edit-post.html', {'form': form})
+class PostEditView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostEditForm
+    template_name = 'edit-post.html'
+    success_url = reverse_lazy('post_list')
 
-    def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
-        form = PostEditForm(request.POST, request.FILES, instance=post)
-        if form.is_valid():
-            form.save()
-            return redirect('post-list')
-        return render(request, 'edit-post.html', {'form': form})
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'delete-post.html'
+    success_url = reverse_lazy('post_list')
